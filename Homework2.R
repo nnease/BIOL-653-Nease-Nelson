54ec6fb2238a0e91b9cdb67a161282c64b97d2ac
Nelson Nease <nelson.nease001@umb.edu>
2015-09-28 14:10:15 -0400

    I changed a few style things so it looks better

--- Homework2.R
+++ Homework2.R
@@ -25,7 +25,8 @@ ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) +
   scale_x_log10() +
   stat_smooth(method = lm)
 
-#closer, been working on it awhile, but can't get it to just one line for the whole graph.....
+#closer, been working on it awhile, but can't get it to just one line for the 
+# whole graph.....
 
 ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) + 
   geom_point(aes(shape = continent)) +
@@ -38,35 +39,44 @@ ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent)) +
   geom_density(aes(shape = continent)) +
   facet_grid(year~lifeExp)
 #^ this one is broken
-ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, fill = continent)) + 
+ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, 
+                             fill = continent)) + 
   geom_density(aes(shape = continent), alpha = 0.4)
 #^ this one is broken
 
-ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, fill = continent)) + 
+ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, 
+                             fill = continent)) + 
   geom_density(color = "black",alpha = 0.4)
 ##^ this one worked, but not faceted
 
-ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, fill = continent)) + 
+ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, 
+                             fill = continent)) + 
   geom_density(color = "black",alpha = 0.4) +
   facet_grid(year~.)
-#^this gets all horizontal facets. dammit. Can I swear in the comments? is that kosher? Fuck it. Sorry about your delicate sensibilities. You ass.
+#^ this gets all horizontal facets. dammit. Can I swear in the comments? 
+#is that kosher? Fuck it. Sorry about your delicate sensibilities. You ass.
 
-ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, fill = continent)) + 
+ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent,
+                             fill = continent)) + 
   geom_density(color = "black",alpha = 0.4) +
   facet_grid(year~)
-#^it's this, but something goes after the ~......
+#^ it's this, but something goes after the ~...... that is not "continent" so 
+# they're grouped.
 
 ggplot(data = gapminder, aes(x = continent, y = lifeExp, colour = continent)) + 
   geom_boxplot() +
-  facet_grid(year~.)
-# again, i think it's right, but wrong facet layout. the dots will squish together?
+  facet_grid(year~continent)
+# again, i think it's right, but wrong facet layout. the dots will squish 
+# together?
 
 
 
 
 
 
-#is there a way to make ggplot and all the things not have to be reloaded everytime? or did i screw myself by starting this thing NOT up here in the script place?
+# is there a way to make ggplot and all the things not have to be reloaded 
+# everytime? or did i screw myself by starting this thing NOT up here in the 
+# script place?
 
 
 
