c4beefb108b37595476d7fc234ee4900929c9be9
Nelson Nease <nelson.nease001@umb.edu>
2015-09-28 17:14:07 -0400

    worked on HW2, now have lab to go to. made good progress, helped by jillian and isaac (isak? how spell?)

--- Homework2.R
+++ Homework2.R
@@ -1,5 +1,5 @@
 #keep the scripts here, to beeter document what is changing
-# load the libraries gapminder and ggplot2
+# load the libraries gapminder and ggplot2 and dplyr and tidyr
 
 ggplot(data = gapminder, aes(x = year, y = lifeExp)) +
   geom_point()
@@ -18,7 +18,7 @@ ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) +
   geom_point(aes(shape = continent)) +
   scale_x_log10()
 
-#easy peasy, took like 5 minutes! ::cackles:: jillian, you were right, I like R
+#^ plot 2
 
 ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) + 
   geom_point(aes(shape = continent)) +
@@ -31,9 +31,9 @@ ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) +
 ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) + 
   geom_point(aes(shape = continent)) +
   scale_x_log10() +
-  geom_smooth(aes(group = 1), size = 2, method = "lm", se = FALSE)
+  geom_smooth(aes(group = 1), size = 1, method = "lm", se = TRUE)
 
-#this is close? i think?
+#^ plot 3
 
 ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent)) + 
   geom_density(aes(shape = continent)) +
@@ -52,24 +52,59 @@ ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent,
 ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, 
                              fill = continent)) + 
   geom_density(color = "black",alpha = 0.4) +
-  facet_grid(year~.)
+  facet_wrap(~year)
 #^ this gets all horizontal facets. dammit. Can I swear in the comments? 
 #is that kosher? Fuck it. Sorry about your delicate sensibilities. You ass.
 
 ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent,
                              fill = continent)) + 
-  geom_density(color = "black",alpha = 0.4) +
-  facet_grid(year~)
-#^ it's this, but something goes after the ~...... that is not "continent" so 
-# they're grouped.
+  geom_density(color = "black",alpha = 0.6) +
+  facet_wrap(~year) 
+#^ plot 4, got it! With Jillian's help. stupid facet wrap.
 
 ggplot(data = gapminder, aes(x = continent, y = lifeExp, colour = continent)) + 
   geom_boxplot() +
   facet_grid(year~continent)
+#^ this is the box plot attempt
 # again, i think it's right, but wrong facet layout. the dots will squish 
-# together?
+# together? need to figure out how to get all the boxes on the same graph?
+
+ggplot(data = gapminder, aes(x = continent, y = lifeExp, colour = continent)) + 
+  geom_boxplot() +
+  facet_wrap(~year) +
+  theme(axis.text.x=element_text(angle=50, size=10, vjust=0.5))
+#^ plot 5 and bonus! y axis labelling fixed.
+
+#v starting dplyer
+
+select(gapminder)
+
+tbl_df(gapminder)
+
+group_by(gapminder, continent)
+
+ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, 
+                              fill = continent)) +
+  geom_density(color = "black", alpha = 0.6)
+#^ plot 6
+
+asian_data <- filter(gapminder, continent == "Asia")
+
+ggplot(data = asian_data, aes(x = lifeExp, y = , colour = continent, 
+                             fill = continent)) +
+  scale_fill_manual(values = c("green")) + 
+  geom_density(color = "black", alpha = 0.6) + 
+  geom_vline(aes(xintercept = mean(lifeExp)))
+#^ plot 7 got it!
 
 
+
+ggplot(data = gapminder, aes(x = lifeExp, y = , colour = continent, 
+                              fill = continent)) +
+  facet_wrap(~continent) + 
+  geom_density(color = "black", alpha = 0.6) + 
+  geom_vline(aes(xintercept = mean(lifeExp)))
+#^this only give the mean for the whole, not the individual countries
 
 
 
